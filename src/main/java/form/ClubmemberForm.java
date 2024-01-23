0cf2e5ca094e6f86563f3b245bacf3f5b0bdbeaf
Yannis Müller <yannis.mueller@sz-ybbs.ac.at>
2024-01-23 08:05:25 +0100

    ClubmemberForm javaa Attribute geändert

--- src/main/java/form/ClubmemberForm.java
+++ src/main/java/form/ClubmemberForm.java
@@ -2,48 +2,64 @@ package form;
 
 public class ClubmemberForm {
 
+    private long id;
     private String name;
-
-    private String description;
-
+    private String surname;
     private int age;
-
+    private String telephoneNumber;
+    private String address;
+    private String role;
     private String websafeClubmemberKey;
 
-    private float alter;
 
     private ClubmemberForm() {}
 
     /**
      * Public constructor is solely for Unit Test.
      * @param name
-     * @param description
      * @param age
      * @param websafeClubmemberKey
      */
 
-    public ClubmemberForm(String name, String description, int age, String websafeClubmemberKey, float alter) {
+    public ClubmemberForm(String name, String surname, int age, String telephoneNumber, String address, String role, String websafeClubmemberKey) {
         this.name = name;
-        this.description = description;
+        this.surname = surname;
         this.age = age;
+        this.telephoneNumber = telephoneNumber;
+        this.address = address;
+        this.role = role;
         this.websafeClubmemberKey = websafeClubmemberKey;
-        this.alter = alter;
+    }
+
+    public long getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getDescription() {
-        return description;
+    public String getSurname() {
+        return surname;
     }
 
     public int getAge() {
         return age;
     }
 
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
     public String getWebsafeClubmemberKey() { return websafeClubmemberKey; }
 
-    public float getAlter() { return alter; }
 }
 
