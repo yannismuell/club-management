bc688bc9d14976ae9ffc07cd64b8d25cc94c2e5e
felixwit <felix.wittmann05@gmail.com>
2025-01-27 08:10:18 +0100

    Changes

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -19,9 +19,9 @@ var app = angular.module('ClubManagementApp', ['clubmanagementControllers', 'ngR
               controller: 'TeamsPageCtrl'
         }).
         when('/teams/members/:websafeTeamKey', {
-                        templateUrl: 'partials/teams_members.html',
-                        controller: 'Teams_membersCtrl'
-                }).
+             templateUrl: 'partials/teams_members.html',
+             controller: 'Teams_membersCtrl'
+        }).
         when('/matches', {
              templateUrl: 'partials/matches.html',
              controller: 'getMatchesCtrl'
